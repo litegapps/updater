92e2e30d5186bf9e9f828f9bedb7f376a8cc19b1
wahyu6070 <gwtsendangrejo6070@gmail.com>
2022-03-21 14:52:50 +0700

    improv

--- update.sh
+++ update.sh
@@ -1,19 +1,16 @@
+SED(){
+	local INPUT=$1
+	local OUTPUT=$2
+	local FILE=$3
+	sed -i 's,'"${INPUT}"','"${OUTPUT}"',g' $FILE
+	}
+
 version=
 versioncode=
 changelog=
 
 
 #litegapps
-varian="
-pixel
-micro
-nano
-basic
-user
-core
-go
-lite
-"
 arch="
 arm
 arm64
@@ -24,4 +21,22 @@ api="
 31
 32
 "
-for 
+
+BASE=`dirname $(readlink -f $0)`
+
+
+for ARCH in $arch; do
+	for API in $api; do
+		for TYPE in MAGISK RECOVERY AUTO; do
+			unset DIR
+			DIR=$BASE/core/litegapps/pixel/$ARCH/$API/$TYPE
+			if [ ! -d $DIR ]; then
+				mkdir -p $DIR
+			else
+				rm -rf $DIR
+				mkdir -p $DIR
+			fi
+			cp -pf $BASE/update.json $DIR/
+		done
+	done
+done
